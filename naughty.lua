f0d69231003c2762ee4d1e75a397836fb67d27a9
Piotr Śliwka <contact@psliwka.info>
2018-05-12 15:32:50 +0200

    Display critical notifications in correct color
    Also, refactor a little

--- naughty.lua
+++ naughty.lua
@@ -11,10 +11,13 @@ local awful = require("awful")
 
 local naughty = {}
 
-naughty.config = { presets = { critical = 1 } }
+naughty.config = { presets = { critical = "critical" } }
 
 function naughty.notify(args)
-    awful.spawn(string.format("notify-send \"%s\" \"%s\"", args["title"], args["text"]))
+    local urgency = naughty.config.presets[args["preset"]]
+    local summary = args["title"]
+    local body = args["text"]
+    awful.spawn(string.format("notify-send -u %s \"%s\" \"%s\"", urgency, summary, body))
 end
 
 return naughty
