4fcce24546a1b5deacda3099ceb6a58d48e11321
Piotr Śliwka <contact@psliwka.info>
2018-05-26 15:13:38 +0200

    Rename rofi module

--- rc.lua
+++ rc.lua
@@ -1,5 +1,5 @@
 local helpers = require("helpers")
-local rofi = require("awesome-rofi")
+local rofi = require("rofi")
 -- Standard awesome library
 local gears = require("gears")
 local awful = require("awful")
