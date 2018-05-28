c133bea0459849d96c93d1522e818ae5811b86bd
Piotr Śliwka <contact@psliwka.info>
2018-05-28 23:20:11 +0200

    Make snapping aids a little nicer to look at

--- rc.lua
+++ rc.lua
@@ -45,6 +45,8 @@ end
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+beautiful.snap_bg = beautiful.border_focus
+beautiful.snap_shape = gears.shape.rectangle
 
 -- This is used later as the default terminal and editor to run.
 terminal = "x-terminal-emulator"
